d98541e743e3df3376d409ffd93f5765b9422f64
saiteja-mandadi-99 <saitejamandadi99@gmail.com>
2024-09-10 06:13:59 +0000

    added spinner feature using react

--- src/components/BlogsList/index.js
+++ src/components/BlogsList/index.js
@@ -1,35 +1,47 @@
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
 import BlogItem from '../BlogItem'
 
 import './index.css'
 
-const blogsData = [
-  {
-    id: 1,
-    title: 'Blog 1',
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-1-img.png',
-    avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
-    author: 'Author Name',
-    topic: 'React.js',
-  },
-  {
-    id: 2,
-    title: 'Blog 2',
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-2-img.png',
-    avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
-    author: 'Author Name',
-    topic: 'React.js',
-  },
-]
-
 class BlogsList extends Component {
+  state = {isLoading: true, blogsData: []}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const statusCode = await response.statusCode
+    console.log(statusCode)
+    const data = await response.json()
+
+    const formattedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogsData: formattedData, isLoading: false})
+  }
+
   render() {
+    const {blogsData, isLoading} = this.state
+    console.log(isLoading)
+
     return (
       <div className="blog-list-container">
-        {blogsData.map(item => (
-          <BlogItem blogData={item} key={item.id} />
-        ))}
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
       </div>
     )
   }
